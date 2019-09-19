ac7edf0d60669d294ca916433e5edbb5206ef81a
Nick <nikita-lyalikov@yandex.ru>
2019-09-19 19:40:43 +0300

    https://leetcode.com/problems/factorial-trailing-zeroes/submissions/

---
+++ FactorialTrailingZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rec(long int deg, int n, int &zrs) {
+        if (n >= deg) {
+            zrs += (n / deg);
+            rec(5*deg, n, zrs);
+        }
+    }
+
+    int trailingZeroes(int n) {
+        int zrs = 0;
+        long int deg = 5;
+        rec(deg, n, zrs);
+        return zrs;
+    }
+};
