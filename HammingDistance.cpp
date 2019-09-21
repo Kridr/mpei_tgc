45e85e5b98bb206aa00a4a5c8c5798778724623e
Nick <nikita-lyalikov@yandex.ru>
2019-09-21 07:36:41 +0300

    https://leetcode.com/problems/hamming-distance/

---
+++ HammingDistance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = x ^ y;
+        int dist = 0;
+
+        while(res) {
+            dist++;
+            res &= res - 1;
+        }
+        return dist;
+    }
+};
